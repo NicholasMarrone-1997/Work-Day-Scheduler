eb310b46d54dd6baa854b9cf3c1ce26b004b3a57
Nicholas Marrone <nmarrone1997@gmail.com>
2021-02-22 14:35:23 -0500

    Reworked JS to a for loop instead of if statements

---
+++ Assets/script.js
@@ -0,0 +1,34 @@
+//Displaying time below the title on webpage
+var time = moment();
+$("#currentDay").text(time.format("MMM Do, YYYY"));
+
+//Gets the current hour
+var currHour = moment();
+var currentHour = parseInt(currHour.format("H"));
+console.log(currentHour);
+
+colorPlanner();
+
+
+function colorPlanner() {
+
+    for (let i = 9; i < 18; i++) {
+        var hourEl = $("#" + i)
+        hourEl.addClass("past");
+
+        if (i < currentHour) {
+            hourEl.addClass("past");
+        } else if (i === currentHour) {
+            hourEl.addClass("present");
+        } else {
+            hourEl.addClass("future");
+        }
+    }
+
+
+    $('.saveBtn').on('click', function () {
+        console.log("clicked");
+        var hourClicked = $(this).attr("data-hour");
+        var text = $("#" + hourClicked).val();
+    })
+}

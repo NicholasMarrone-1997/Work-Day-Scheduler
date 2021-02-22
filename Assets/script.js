e8c3d65e09aaee652725066ac00730b04fccaa58
Nicholas Marrone <nmarrone1997@gmail.com>
2021-02-22 15:45:09 -0500

    Fixed key, value pair functionality in local storage

--- Assets/script.js
+++ Assets/script.js
@@ -3,8 +3,8 @@ var time = moment();
 $("#currentDay").text(time.format("MMM Do, YYYY"));
 
 //Gets the current hour
-var currHour = moment();
-var currentHour = parseInt(currHour.format("H"));
+//var currHour = moment();
+var currentHour = parseInt(currHour.moment().format("H"));
 console.log(currentHour);
 
 colorPlanner();
@@ -12,9 +12,9 @@ colorPlanner();
 
 function colorPlanner() {
 
+    //Looping through work day schudler hours and add it to a # symbol and then comparing hour to currenthour and adding class
     for (let i = 9; i < 18; i++) {
         var hourEl = $("#" + i)
-        hourEl.addClass("past");
 
         if (i < currentHour) {
             hourEl.addClass("past");
@@ -25,13 +25,14 @@ function colorPlanner() {
         }
     }
 
-
-    $('.saveBtn').on('click', function () {
+    //Saves data-hour and input text as key, val pair on save button click
+    $('.saveBtn').on("click", function () {
         console.log("clicked");
         var hourClicked = $(this).attr("data-hour");
         var text = $("#" + hourClicked).val();
-
-        localStorage.setItem("button1", hourClicked);
-        localStorage.getItem("button1");
+        console.log(text);
+        localStorage.setItem(hourClicked, text);
     })
+
+    
 }
